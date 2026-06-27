5f2960ef077eb0407f071ae4f7ba0cddae1836c5
yasmine <yasminee455@gmail.com>
2026-06-28 00:28:08 +0300

    init travel details

--- massar/lib/travel_detail_screen.dart
+++ massar/lib/travel_detail_screen.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:massar/custom%20widgets/bottom_nav_glass.dart';
+import 'package:massar/custom%20widgets/glass_button.dart';
+
+
 
 class TravelDetailScreen extends StatelessWidget {
   const TravelDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Background gradient
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color(0xFF00287C),
+                  Color(0xFF01253D),
+                  Color(0xFF00133F),
+                ],
+                stops: [0.0, 0.45, 1.0],
+              ),
+            ),
+          ),
+
+          // Glow
+          Container(
+            decoration: BoxDecoration(
+              gradient: RadialGradient(
+                center: const Alignment(-0.9, 0.9),
+                radius: 1.15,
+                colors: [
+                  const Color(0xFFBAD1FF).withOpacity(0.28),
+                  const Color(0xFFBAD1FF).withOpacity(0.08),
+                  Colors.transparent,
+                ],
+                stops: const [0.0, 0.55, 1.0],
+              ),
+            ),
+          ),
+        
+          
+        ],
+      ),
+    );
   }
 }
