94657b2445227f48d1299e2c64305ae02126fc39
yasmine <yasminee455@gmail.com>
2026-06-28 17:11:07 +0300

    prices

--- massar/lib/travel_detail_screen.dart
+++ massar/lib/travel_detail_screen.dart
@@ -281,7 +281,35 @@ class TravelDetailScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                
+                GlassContainer(
+                  width: double.infinity,
+                  height: 54,
+                  borderRadius: 20,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Economy class',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          '3000 EGP',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
