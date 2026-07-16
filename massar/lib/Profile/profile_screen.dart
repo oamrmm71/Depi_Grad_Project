1cdf7c3912d6b33c1499f15d4986057e12ebbd33
oamrmm71 <omarmourt2004@email.com>
2026-07-16 18:11:26 +0300

    edits

--- massar/lib/Profile/profile_screen.dart
+++ massar/lib/Profile/profile_screen.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
+import 'package:massar/Profile/services/profile_service.dart';
 import 'package:massar/theme/app_colors.dart';
 import 'package:massar/custom%20widgets/bottom_nav_glass.dart';
 import 'package:massar/custom%20widgets/glass_container.dart';
